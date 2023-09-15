bea23ca12a806516ddc0c0389a6c6e5224928a91
theee-a-team <129443389+theee-a-team@users.noreply.github.com>
2023-09-15 21:01:51 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+document.getElementById("start").addEventListener("click", function() {
+    document.getElementById("eyes1").classList.add("eyes")
+    document.getElementById("eyes2").classList.add("eyes")
+    setTimeout(overlayFadeOut, 500)
+    setInterval(backgroundChanger, 7500);
+});
+
+function overlayFadeOut() {
+    $("#overlay").fadeOut(600);
+}
+
+var images = [
+    'url("img/1.jpg")',
+    'url("img/2.jpg")',
+    'url("img/3.jpg")',
+    'url("img/4.jpg")',
+    'url("img/5.jpg")',
+    'url("img/6.jpg")',
+    'url("img/7.jpg")',
+    'url("img/8.jpg")',
+    'url("img/9.jpg")',
+]
+    
+function backgroundChanger() {
+        $("#test").fadeOut(750, () => {document.getElementById('test').style.backgroundImage = images[Math.floor(Math.random() * 10)]});
+        $("#test").fadeIn(750);
+    } 
+        
+
+
+
